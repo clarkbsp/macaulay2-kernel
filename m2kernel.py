626835a42a7c55f5e8d07d71f5a5470b2a7e4937
clark <clark@gigaknight>
2017-11-10 00:11:55 -0500

    fixed newline parse bug

--- m2kernel.py
+++ m2kernel.py
@@ -10,6 +10,7 @@ from ipykernel.kernelbase import Kernel
 
 import pexpect
 import re
+import signal
 
 child = pexpect.spawnu("M2")
 
@@ -17,9 +18,9 @@ child.expect ('i\d+ :')
 
 print('\n'.join(child.before.split('\r\n')[:-1]))
 
+#signal.signal(signal.SIGINT,print("interupt_received"))
 
 class M2Kernel(Kernel):
-    
     
     implementation = 'Macaulay2'
     implementation_version = '0.01'
@@ -28,15 +29,16 @@ class M2Kernel(Kernel):
     language_info = {'name': 'Macaulay2', 
                      'mimetype': 'text/plain'}
     banner = "Macaulay2"
- 
+    
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=False):
+        code = '\n'.join(re.findall('(?:"[^"]*"|.)+', code))#Collapses newlines in code
         if not silent:
             child.send(code+'\n'+'\r\n\r\n') 
             child.expect(['\r\n\s+\r\n\s+\r\n\s+'])
             
             #Output should probably be filtered/formatted
-            result = '\n'.join(child.before.split('\r\n')[1:-1])
+            result = '\n'.join(child.before.split('\r\n')[:-1])
             stream_content = {'name': 'stdout', 'text': result}
             self.send_response(self.iopub_socket, 'stream', stream_content)
 
